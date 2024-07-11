ace3fa419d28f42fd84b9c466f18e426d4a06aa7
Ridwan <ridwanyinusa15@gmail.com>
2024-07-11 16:09:30 +0100

    a

--- app/Rooms/RoomHero.tsx
+++ app/Rooms/RoomHero.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const RoomHero = () => {
   return (
     <main className="sm:pb-20 lg:pb-28 largesceen:pb-44 overflow-hidden">
-      <section className="relative w-full  h-full max-sm:h-[60vh]">
+      <section className="relative w-full  h-full max-sm:h-[65vh]">
         <Image
           src={best}
           alt="diver"
@@ -15,7 +15,7 @@ const RoomHero = () => {
         />
 
         <div className="relative px-5 md:px-[1.9rem] largesceen:px-14 fourk:px-44 pt-12 desktop:pt-28 largesceen:pt-44">
-          <h2 className="text-secondary  text-[3.2rem] leading-none lg:text-[6.25rem] desktop:text-[7.813rem] largesceen:text-[9.375rem] mb-12 sm:mb-28 lg:mb-36">
+          <h2 className="text-secondary  text-[3.2rem] leading-none lg:text-[6.25rem] desktop:text-[7.813rem] largesceen:text-[9.375rem] mb-20 sm:mb-28 lg:mb-36">
             Superior <br className="hidden sm:block" /> twin
           </h2>
           <p className="text-secondary text-[0.813rem] desktop:text-base 2xl:text-lg leading-[155.556%] w-[63%] sm:w-[300px] desktop:w-[27.625rem] uppercase">
@@ -23,7 +23,7 @@ const RoomHero = () => {
           </p>
         </div>
 
-        <div className="absolute right-0 xs:-bottom-0 sm:-bottom-44 md:-bottom-28 w-24 sm:w-48 sm:h-48 small:m-5 label">
+        <div className="absolute right-0 bottom-2 sm:-bottom-44 md:-bottom-28 w-24 sm:w-48 sm:h-48 small:m-5 label">
           <a href="/" className="flex items-center justify-center  no-underline">
             <object type="image/svg+xml" data="/Polygon 5.svg" className="max-xs:h-[60px] xs:w-[80px] md:w-[120px]">
               Your browser does not support SVGs
