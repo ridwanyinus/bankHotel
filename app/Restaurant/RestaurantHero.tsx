757665c8b4670181d9658ea8ff870c62a91d5ad2
Ridwan <ridwanyinusa15@gmail.com>
2024-07-12 11:58:53 +0100

    a

--- app/Restaurant/RestaurantHero.tsx
+++ app/Restaurant/RestaurantHero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import labela from "@/public/labelr.svg";
 import Image from "next/image";
+import restaurant from "@/public/restaurant1.png";
+import restaurant2 from "@/public/space-copenhagen-the-stratford-architonic-jw-5-17-arcit18 1.png";
 
 const RestaurantHero = () => {
   return (
@@ -18,6 +20,29 @@ const RestaurantHero = () => {
           <Image src={labela} alt="label" className="absolute w-[5.313rem]  -top-14 -left-14  label" />
         </div>
       </div>
+
+      <div className="flex flex-col md:flex-row justify-center items-center mt-12 sm:mt-20 md:mt-28 lg:mt-32 md:space-x-5 largesceen:gap-x-10">
+        <Image
+          src={restaurant}
+          alt="restaurant"
+          className="w-[80%] md:w-[22.25rem] lg:h-[28.125rem] lg:w-[37.5rem]  desktop:h-[38rem] desktop:w-[55rem] largesceen:h-[50rem] largesceen:w-[76.875rem]"
+        />
+        <Image
+          src={restaurant2}
+          alt="restaurant"
+          className="w-[70%] lg:w-[22.25rem] lg:h-[22.25rem]  desktop:w-[24.125rem] desktop:h-[28.125rem] largesceen:w-[37.5rem] largesceen:h-[37.5rem] object-cover mt-5 md:mt-0"
+        />
+      </div>
+
+      <div className=" mt-12 sm:mt-20 md:mt-28 xl:mt-32">
+        <h1 className="text-white-100 capitalize  text-2xl  xl:text-3xl desktop:text-4xl largesceen:text-[3.125rem] ">At your service</h1>
+
+        <p className="text-white-100 text-[0.813rem] desktop:text-base 2xl:text-lg leading-[155.556%] sm:w-[15.625rem] desktop:w-[21.875rem] largesceen:w-[25rem] text-opacity-[0.8] mt-8 lg:mt-10 desktop:mt-12 largesceen:mt-16">
+          The team of the Safe Restaurant aims to exceed all expectations of our guests. Our chef worked hard to develop a unique menu that features the best meals of the European cuisine that will
+          match the tastes of the most demanding clients. Friendly and attentive waiters will ensure that you will enjoy your time in our restaurant.
+        </p>
+      </div>
+      
       
       
       
