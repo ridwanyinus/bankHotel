fc34207550c5ddfc170a12a3ea14b2eeecd5def3
Ridwan <ridwanyinusa15@gmail.com>
2024-12-23 11:49:20 +0100

    chore(metadata): update metadata

--- app/Restaurant/page.tsx
+++ app/Restaurant/page.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import Navbars from "@/components/Navbar";
-import RestaurantHero from "./RestaurantHero";
-import RestaurantFooter from "./RestaurantFooter";
+import React from 'react';
+import Navbars from '@/components/Navbar';
+import RestaurantHero from './RestaurantHero';
+import RestaurantFooter from './RestaurantFooter';
+
+export const metadata = {
+  title: 'Restaurant Bank Hotel - Bank Hotel Website by Ridwan',
+};
+
 const page = () => {
   return (
-    <main className="bg-primary">
+    <main className='bg-primary'>
       <Navbars />
       <RestaurantHero />
       <RestaurantFooter />
